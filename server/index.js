31d964a0f873b6d8c164f36ce8ccb526c08bca23
satowine1 <marcobgg4@gmail.com>
2017-06-26 22:23:52 +0200

    Definizione scheletro handling degli errori

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 //Init Services, Database & logic var
 
+  //ErrorHandler
+  //var error = require('./controller/middleware/errorHandler');
+
   //moustache
   const mu = require('./controller/middleware/parse');
 
@@ -17,6 +20,9 @@
 
   //BodyParser init
   app.use(bodyParser.json());
+
+  //Handling degli errori
+  //definire app.use(error.nomefunzione())
 
   //port
   const port = 3000;
@@ -62,119 +68,4 @@ app.listen(port, ()=>{
       res.send(parsed);
     });
   })
-
-/* //moustache
-const mu = require('./utility/parser');
-
-//forge
-var encr = require('./utility/encrypt');
-
-//db
-var db = require('./model/database_logic');
-
-//express
-const express = require('express');
-var bodyParser = require('body-parser');
-const app = express();
-
-//port
-const port = 3000;
-
-//BodyParser init
-app.use(bodyParser.json());
-
-//global var
-var keyCrypt;
-var ivCrypt;
-var encripted;
-var errRead = "error loading request parameters";
-
-//global Fun
-function loadCryptParam(){
-  db.load_keyCrypt(function(err, res){
-    if(err){
-      console.log(err);
-    }
-    else{
-      keyCrypt = res[0].key_code;
-      ivCrypt = res[0].iv_code;
-      console.log("parameters loaded correctly");
-    }
-  })
-}
-
-//DEBUG & TESTING
-app.post("/test", function(req, res){
-  var key = encr.get_key();
-  var iv = encr.get_iv();
-  db.ins_crypt_param(key, iv, function(){
-    loadCryptParam();
-  });
-})
-
-//Express routing
-
-  //Init Server
-  app.post("/start", function(req, res){
-      //generate crypt Param
-      db.load_keyCrypt(function (err,res) {
-        if(!err && res == ""){ //first time -> create param, insert them in db and retry to load them
-          var key = encr.get_key();
-          var iv = encr.get_iv();
-          db.ins_crypt_param(key, iv, function(){
-            console.log("parameters created correctly");
-            loadCryptParam();
-          });
-        }
-        else if(!err){ // param already exist -> simply load them
-          loadCryptParam();
-        }
-        else if(err){ // trouble with loading
-          console.log(errRead);
-        }
-    });
-  })
-
-  //Db request
-  app.post("/drop", function(req, res){
-    db.drop_schema();
-    res.send("Schema dropped");
-  })
-
-  //Utility Func
-    //Encrypt/Decrypt
-      app.post("/encrypt", function(req,res){
-        var myBytes = JSON.stringify(req.body);
-        console.log(myBytes);
-        encripted = encr.encrypt(myBytes, keyCrypt, ivCrypt);
-        console.log("File cripted correctly");
-        res.send(encripted.data);
-      })
-      app.post("/decrypt", function(req,res){
-        //console.log(encripted);
-        var decripted = encr.decrypt(encripted, keyCrypt, ivCrypt);
-        console.log("File decripted correctly");
-        res.send(decripted);
-      })
-    //Parsing/Generate
-      app.post("/parsing", function(req,res) { //modifica per il return e fai un callback
-        var myMu = req.body;
-        mu.parsing(myMu, function(parsed){
-          res.send(parsed);
-        });
-      })
-
-
-
-
-
-
-  --commenta--app.post('/ins_utente', function(req,res){
-
-    db.ins_user({username: req.body.username, password: req.body.password}, function(err, data) {
-
-
-
-    });
-  }) --commenta--*/
 
